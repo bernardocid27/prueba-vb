6824bf03ccfda9d328a410a6aa89d0844e26b6d5
Jose Cid <bernardocid27@gmail.com>
2020-09-07 20:28:27 -0400

    Agregar archivos de proyecto.

---
+++ prueba vb/Form1.Designer.vb
@@ -0,0 +1,175 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtid = New System.Windows.Forms.TextBox()
+        Me.txtnombre = New System.Windows.Forms.TextBox()
+        Me.txtapellido = New System.Windows.Forms.TextBox()
+        Me.txtedad = New System.Windows.Forms.TextBox()
+        Me.btnInsertar = New System.Windows.Forms.Button()
+        Me.btnActualizar = New System.Windows.Forms.Button()
+        Me.btnEliminar = New System.Windows.Forms.Button()
+        Me.txtBuscar = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(129, 53)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(16, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Id"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(129, 91)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(47, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Nombre:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(129, 136)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(47, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Apellido:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(129, 177)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(35, 13)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Edad:"
+        '
+        'txtid
+        '
+        Me.txtid.Location = New System.Drawing.Point(212, 53)
+        Me.txtid.Name = "txtid"
+        Me.txtid.Size = New System.Drawing.Size(100, 20)
+        Me.txtid.TabIndex = 4
+        '
+        'txtnombre
+        '
+        Me.txtnombre.Location = New System.Drawing.Point(212, 91)
+        Me.txtnombre.Name = "txtnombre"
+        Me.txtnombre.Size = New System.Drawing.Size(100, 20)
+        Me.txtnombre.TabIndex = 5
+        '
+        'txtapellido
+        '
+        Me.txtapellido.Location = New System.Drawing.Point(212, 136)
+        Me.txtapellido.Name = "txtapellido"
+        Me.txtapellido.Size = New System.Drawing.Size(100, 20)
+        Me.txtapellido.TabIndex = 6
+        '
+        'txtedad
+        '
+        Me.txtedad.Location = New System.Drawing.Point(212, 177)
+        Me.txtedad.Name = "txtedad"
+        Me.txtedad.Size = New System.Drawing.Size(100, 20)
+        Me.txtedad.TabIndex = 7
+        '
+        'btnInsertar
+        '
+        Me.btnInsertar.Location = New System.Drawing.Point(383, 53)
+        Me.btnInsertar.Name = "btnInsertar"
+        Me.btnInsertar.Size = New System.Drawing.Size(75, 23)
+        Me.btnInsertar.TabIndex = 8
+        Me.btnInsertar.Text = "INSERTAR"
+        Me.btnInsertar.UseVisualStyleBackColor = True
+        '
+        'btnActualizar
+        '
+        Me.btnActualizar.Location = New System.Drawing.Point(383, 89)
+        Me.btnActualizar.Name = "btnActualizar"
+        Me.btnActualizar.Size = New System.Drawing.Size(75, 23)
+        Me.btnActualizar.TabIndex = 9
+        Me.btnActualizar.Text = "ACTUALIZAR"
+        Me.btnActualizar.UseVisualStyleBackColor = True
+        '
+        'btnEliminar
+        '
+        Me.btnEliminar.Location = New System.Drawing.Point(383, 126)
+        Me.btnEliminar.Name = "btnEliminar"
+        Me.btnEliminar.Size = New System.Drawing.Size(75, 23)
+        Me.btnEliminar.TabIndex = 10
+        Me.btnEliminar.Text = "ELIMINAR"
+        Me.btnEliminar.UseVisualStyleBackColor = True
+        '
+        'txtBuscar
+        '
+        Me.txtBuscar.Location = New System.Drawing.Point(383, 13)
+        Me.txtBuscar.Name = "txtBuscar"
+        Me.txtBuscar.Size = New System.Drawing.Size(75, 23)
+        Me.txtBuscar.TabIndex = 11
+        Me.txtBuscar.Text = "BUSCAR"
+        Me.txtBuscar.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(579, 323)
+        Me.Controls.Add(Me.txtBuscar)
+        Me.Controls.Add(Me.btnEliminar)
+        Me.Controls.Add(Me.btnActualizar)
+        Me.Controls.Add(Me.btnInsertar)
+        Me.Controls.Add(Me.txtedad)
+        Me.Controls.Add(Me.txtapellido)
+        Me.Controls.Add(Me.txtnombre)
+        Me.Controls.Add(Me.txtid)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents txtid As TextBox
+    Friend WithEvents txtnombre As TextBox
+    Friend WithEvents txtapellido As TextBox
+    Friend WithEvents txtedad As TextBox
+    Friend WithEvents btnInsertar As Button
+    Friend WithEvents btnActualizar As Button
+    Friend WithEvents btnEliminar As Button
+    Friend WithEvents txtBuscar As Button
+End Class
---
+++ prueba vb/Form1.vb
@@ -0,0 +1,94 @@
+﻿Imports System.Data.SqlClient
+
+Public Class Form1
+
+    Dim connection As New SqlConnection("Server=DESKTOP-R3DPSGI\SQLEXPRESS; Database = prueba; Integrated Security = true")
+    Private Sub btnInsertar_Click(sender As Object, e As EventArgs) Handles btnInsertar.Click
+
+        Dim insertQuery As String = "INSERT INTO persona (nombre,apellido,edad) VALUES ('" & txtnombre.Text & "','" & txtapellido.Text & "','" & txtedad.Text & "')"
+
+        ExecuteQuery(insertQuery)
+
+        MessageBox.Show("Insertados correctamente")
+
+        txtnombre.Clear()
+        txtapellido.Clear()
+        txtedad.Clear()
+
+
+    End Sub
+
+    Public Sub ExecuteQuery(query As String)
+        Dim command As New SqlCommand(query, connection)
+
+        connection.Open()
+
+        command.ExecuteNonQuery()
+
+        connection.Close()
+
+    End Sub
+
+    Private Sub btnActualizar_Click(sender As Object, e As EventArgs) Handles btnActualizar.Click
+
+        Dim updateQuery As String = "UPDATE persona SET nombre='" & txtnombre.Text & "', apellido='" & txtapellido.Text & "', edad='" & txtedad.Text & "' WHERE id=" & txtid.Text & " "
+
+        ExecuteQuery(updateQuery)
+
+        MessageBox.Show("Datos actualizados")
+
+        txtid.Clear()
+        txtnombre.Clear()
+        txtapellido.Clear()
+        txtedad.Clear()
+
+    End Sub
+
+    Private Sub btnEliminar_Click(sender As Object, e As EventArgs) Handles btnEliminar.Click
+
+        Dim deleteQuery As String = "DELETE FROM persona WHERE id=" & txtid.Text
+
+        ExecuteQuery(deleteQuery)
+
+        MessageBox.Show("Datos Eliminados")
+
+        txtid.Clear()
+        txtnombre.Clear()
+        txtapellido.Clear()
+        txtedad.Clear()
+    End Sub
+
+    Private Sub txtBuscar_Click(sender As Object, e As EventArgs) Handles txtBuscar.Click
+
+        Dim command As New SqlCommand("SELECT * FROM persona WHERE id= @id", connection)
+
+        command.Parameters.Add("@id", SqlDbType.Int).Value = txtid.Text
+
+        Dim adapter As New SqlDataAdapter(command)
+
+        Dim table As New DataTable()
+
+        adapter.Fill(table)
+
+        If table.Rows.Count() > 0 Then
+
+            txtnombre.Text = table(0)(1).ToString()
+            txtapellido.Text = table(0)(2).ToString()
+            txtedad.Text = table(0)(3).ToString()
+
+        Else
+
+            MessageBox.Show("No Existe Datos")
+
+
+            txtnombre.Clear()
+            txtapellido.Clear()
+            txtedad.Clear()
+
+        End If
+
+
+
+
+    End Sub
+End Class
